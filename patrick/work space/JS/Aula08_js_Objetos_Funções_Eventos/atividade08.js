21e717c4a90f760d56f5ed4e957b0d142bea84ff
patricks10 <patrickscano@gmail.com>
2023-05-18 21:16:48 -0300

    Aula JS 08 objetos funções e eventos

---
+++ patrick/work space/JS/Aula08_js_Objetos_Funções_Eventos/atividade08.js
@@ -0,0 +1,11 @@
+const boasVindas = () => {
+    alert('Bem vindo a página da atividade 08');
+    console.log('Bem vindo a página da atividade 08');
+};
+
+const conta = (a,b,c) => {
+    const delta = b * b - 4 * a * c;
+const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+console.log('valor do x1', x1,'Valor do x2',x2)
+};
