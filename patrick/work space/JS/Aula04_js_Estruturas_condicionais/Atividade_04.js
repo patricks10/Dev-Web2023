faf2423590390a58158dbbcb418a59793f4064e8
patricks10 <patrickscano@gmail.com>
2023-06-01 21:35:23 -0300

    Atividade aula 04 estruturas condicionais

---
+++ patrick/work space/JS/Aula04_js_Estruturas_condicionais/Atividade_04.js
@@ -0,0 +1,13 @@
+var numero1 = prompt("digite o primeiro número:")
+var numero2 = prompt("digite o segundo número:")
+
+    if (numero1 > numero2) {
+        console.log(numero1 + " Numero1 é maior que número2");
+    } else if(numero1 < numero2) {
+        console.log(numero2 + " Número2 é maior que número1");
+    } else{
+        console.log("Os números digitados são iguais")
+    }
+
+
+verificarNumero()
