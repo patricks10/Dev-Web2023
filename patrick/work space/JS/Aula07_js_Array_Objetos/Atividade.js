9b1c0cb40c7ddd7ce37714fa97e13cfd2a8c8ba7
patricks10 <patrickscano@gmail.com>
2023-06-13 15:38:38 -0300

    Atividade aula 07 js array de objetos

---
+++ patrick/work space/JS/Aula07_js_Array_Objetos/Atividade.js
@@ -0,0 +1,43 @@
+const aluno = [
+    {
+      nome: "Ana",
+      idade: 17,
+      nota: 8,
+      ano: "2ºB",
+    },
+    {
+      nome: "Bruno",
+      idade: 16,
+      nota: 6,
+      ano: "2ºC",
+    },
+    {
+      nome: "Veronica",
+      idade: 16,
+      nota: 9,
+      ano: "2ºC",
+    },
+    {
+      nome: "Marta",
+      idade: 15,
+      nota: 5,
+      ano: "3ºC",
+    },
+    {
+      nome: "Brenno",
+      idade: 19,
+      nota: 6,
+      ano: "3ºC",
+    },
+    {
+      nome: "Maria",
+      idade: 14,
+      nota: 4,
+      ano: "1ºF",
+    },
+  ]
+  
+  console.log(aluno)
+  console.log(aluno[1])
+  console.log(aluno[3].nome)
+  console.log(JSON.stringify(aluno))
